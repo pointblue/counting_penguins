ef3c3f008dd583be224577db0eb93389f32d6d4a
aeschmidt <aschmidt@pointblue.org>
2022-08-25 15:48:31 -0700

    changed name of script to create pick list

--- scripts/create_tile_pick_list.R
+++ scripts/create_tile_pick_list.R
@@ -9,8 +9,15 @@ library(aws.s3)
 
 
 # create list to pick from ------------------------------------------------
-bucket = "s3://pb-adelie/"
-prefix = "1920_UAV_survey/orthomosaics/croz/191202/croz_20191202_tiles/"
+bucket <-
+  "s3://pb-adelie/"
+
+prefix <-
+  "1920_UAV_survey/orthomosaics/croz/191202/croz_20191202_tiles/"
+
+# set desired name of picklist
+pl_name <-
+  "croz_20191202_pick_list.csv"
 
 files <- 
   get_bucket_df(
@@ -18,16 +25,6 @@ files <-
     prefix = prefix,
     max = Inf
   )
-
-# 
-# files <-
-#   files %>%
-#   mutate(tile_name = str_replace(Key, patter = prefix, replacement = ""),
-#     x = str_extract(tile_name, pattern  = "(?<=^.{14})(\\d{1,3})"),
-#          y = str_extract(tile_name, pattern  = "(\\d{1,3})(?=\\.jpg)"))
-
-# plot(files$x, files$y, pch = 19, cex=0.1, xlim = c(0,10),ylim = c(0,10))
-#
 
 # filter to remove tiles with low probability of penguins
 files_filt <-
@@ -52,37 +49,27 @@ pick_list <-
   filter(files_filt,tileName %in% samp) %>%
   select(tileName)
 
-# # make a copy of files in new location in s3
-# for(i in 1:nrow(pick_list)) {
-#   copy_object(
-#     from_object = paste0(prefix,pick_list[i,1]),
-#                          to_object = paste0(prefix,"validation_set/",pick_list[i,1]),
-#                         from_bucket = bucket,
-#                        to_bucket = bucket
-#               )
-# }
-
-
 # create table with tile name and x y coordinates
 pick_list_df <- 
   pick_list %>%
   # add columns for coordinates
   mutate(
-    # x = str_extract(tile_name, pattern  = "(?<=^.{14})(\\d{1,3})"),
-    #      y = str_extract(tile_name, pattern  = "(\\d{1,3})(?=\\.jpg)"),
-         # add column for whether tile has been processed and by whom
-         tagged = NA,
-         initials = NA,
-         datetime = NA
+    # add column for whether tile has been processed and by whom
+    downloaded = 0,
+    tagged = 0,
+    initials = NA,
+    datetime = NA
   )
+
 # write picklist to s3
 s3write_using(pick_list_df,
               FUN = write_csv, 
-              object = paste0(prefix,"validation_set/croz_20191202_pick_list.csv"),
+              object = paste0(prefix, pl_name),
               bucket = bucket)
 
 # create table with labels for YOLO
-yolo_labs = c("ADPE_a", "ADPE_a_stand", "ADPE_j")
+# these need to match the labels in the model (except for no_penguin which is not in the model)
+yolo_labs = c("ADPE_a", "ADPE_a_stand", "ADPE_j", "no_penguin")
 
 labs <- 
   data.frame(yolo_labs)
@@ -90,10 +77,6 @@ s3write_using(labs,
               FUN = write_delim,
               col_names = FALSE,
               delim = ",",
-              object = paste0(prefix,"validation_set/label_key.txt"),
+              object = paste0(prefix,"label_key.txt"),
               bucket = bucket)
               
-
-
-
-plot(pick_list_df$x, pick_list_df$y)
